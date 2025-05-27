c464f1385273ce040e5c322e0c50c787fdc85f06
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-27 15:51:24 +0900

    chore: add GlobalNavigationLoadingBar

--- app/root.client.tsx
+++ app/root.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useNavigation } from "react-router";
 
 export function ServerHmr() {
   if (import.meta.hot) {
@@ -16,4 +16,16 @@ export function ServerHmr() {
     }, [navigate]);
   }
   return null;
+}
+
+export function GlobalNavigationLoadingBar() {
+  const navigation = useNavigation();
+
+  if (navigation.state === "idle") return null;
+
+  return (
+    <div className="h-1 w-full bg-pink-100 overflow-hidden fixed top-0 left-0 z-50 opacity-50">
+      <div className="animate-progress origin-[0%_50%] w-full h-full bg-pink-500" />
+    </div>
+  );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import { Favorites } from "./components/favorites-bar.tsx";
 import { sessionMiddleware } from "./session.ts";
 import { dataMiddleware } from "./db.ts";
 import { Resources } from "@hiogawa/vite-rsc/rsc"
-import { ServerHmr } from "./root.client.tsx";
+import { GlobalNavigationLoadingBar, ServerHmr } from "./root.client.tsx";
 
 export const unstable_middleware: MiddlewareFunction<Response>[] = [
   sessionMiddleware,
@@ -39,6 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <Favorites />
         <ScrollRestoration />
+        <GlobalNavigationLoadingBar />
         <ServerHmr />
       </body>
     </html>
